3150f1dd23a2850d8aeecdb518fe8c1ea2274ef3
yamashi4423 <yamashi4423@gmail.com>
2023-11-24 14:47:05 +0900

    useDidUpdateEffectを追加

---
+++ src/hooks/useDidUpdateEffect.ts
@@ -0,0 +1,16 @@
+// https://zenn.dev/catnose99/scraps/30c623ba72d6b5
+
+import { useRef, useEffect, EffectCallback, DependencyList } from "react";
+
+// 初回の実行がスキップされるuseEffect
+export function useDidUpdateEffect(fn: EffectCallback, deps: DependencyList) {
+  const didMountRef = useRef(false);
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+    } else {
+      fn();
+    }
+  }, deps);
+}
