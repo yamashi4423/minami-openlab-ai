dddc77dd11b81901f41a278c4a60555b8128b7c3
yamashi4423 <yamashi4423@gmail.com>
2024-03-26 13:13:45 +0900

    imageUrlの画像を差し替え

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -4,7 +4,7 @@ export const Meta = () => {
   const title = "ChatMinamiLab";
   const description =
     "電気通信大学の南研究室を紹介する対話システムです。南研について気軽にお聞きください！この対話システムは調布祭（文化祭）用のオープンラボ用に作られました。";
-  const imageUrl = "/screenshot.png";
+  const imageUrl = "/screenshot2.png";
   return (
     <Head>
       <title>{title}</title>
